923e637e111cf287869ffc121de8d2ccc835e021
r-kimura <ryuumockingbird@gmail.com>
2023-05-07 12:44:37 +0900

    ♻️ Mainをコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -7,8 +7,8 @@ export function Headline(props) {
 
   return (
     <>
-      <h1> { props.page } page</h1>
-      <p> Get started by editing </p>
+      <h1> {props.page} page</h1>
+      <p> Get started by editing {props.children}</p>
       <button onClick={props.onClick}>ボタン</button>
     </>
   );

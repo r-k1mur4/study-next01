f1ed7f359fc7ad02513edba4dae5bdb4c26ee1a5
Ryuki Kmura <ryuumockingbid@gmail.com>
2023-05-09 11:18:51 +0900

    ♻️ add map

--- components/List.js
+++ components/List.js
@@ -1,8 +1,64 @@
 
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    title: "Docs",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    title: "Learn",
+    description:
+      "Learn about Next.js in an interactive course with&nbsp;quizzes!",
+  },
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    title: "Docs",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    title: "Templates",
+    description:
+      "Discover and deploy boilerplate example Next.js&nbsp;projects.",
+  },
+  // {
+  //   href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+  //   title: "Deploy",
+  //   description:
+  //     "Instantly deploy your Next.js site to a shareable URL with Vercel.",
+  // },
+];
+
 export function List(){
   return (
     <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-      <a
+      {ITEMS.map((item) => {
+        return (
+          <a
+            key={item.href}
+            href={item.href}
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2
+              key={item.title}
+              className={`mb-3 text-2xl font-semibold`}>
+              {item.title}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p key={item.description} className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              {item.description}
+            </p>
+          </a>
+        );
+      })}
+
+
+      {/* <a
         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         target="_blank"
@@ -35,7 +91,6 @@ export function List(){
           Learn about Next.js in an interactive course with&nbsp;quizzes!
         </p>
       </a>
-
       <a
         href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -68,7 +123,7 @@ export function List(){
         <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
           Instantly deploy your Next.js site to a shareable URL with Vercel.
         </p>
-      </a>
+      </a> */}
     </div>
   );
 };
